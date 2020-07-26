61704e1163eba5c8cd62c7648b8e93ecd36c08fb
zixiangma <994890576@qq.com>
2020-07-27 00:11:07 +0800

    添加测试类2.3

---
+++ src/main/java/com/mzx/Test2.java
@@ -0,0 +1,7 @@
+package com.mzx;
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("这是第二个测试类");
+    }
+}
---
+++ src/main/java/com/mzx/Test3.java
@@ -0,0 +1,7 @@
+package com.mzx;
+
+public class Test3 {
+    public static void main(String[] args) {
+        System.out.println("这是第三个测试类");
+    }
+}
